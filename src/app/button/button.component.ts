a1bbba5251095b45f53f5507ebdc6a381088646b
Michael Small <chickenhat22@gmail.com>
2024-10-13 12:10:34 -0500

    refactor: move basic button settings to own component + random vals for padding

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component, model, signal} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from "@angular/material/divider";
 import {MatIconModule} from "@angular/material/icon";
@@ -6,20 +6,13 @@ import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {FormsModule} from "@angular/forms";
 import {MatOption, MatSelect} from "@angular/material/select";
+import {BasicControlsComponent} from "./controls/basic-controls.component";
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule, MatSelect, MatOption],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule, MatSelect, MatOption, BasicControlsComponent],
   template: `
-    <section>
-      <button mat-button>Basic</button>
-      <button mat-button disabled>Disabled</button>
-      <a mat-button href="https://www.google.com/" target="_blank">Link</a>
-    </section>
-
-    <mat-divider />
-
     <section>
       <button mat-raised-button>Basic</button>
       <button mat-raised-button disabled>Disabled</button>
@@ -42,38 +35,11 @@ import {MatOption, MatSelect} from "@angular/material/select";
       <a mat-flat-button href="https://www.google.com/" target="_blank">Link</a>
     </section>
 
-    <section id="Basic Button">
-      <h2>Basic Button Settings</h2>
-      <mat-form-field>
-        <mat-label>mdc_text_button_label_text_color</mat-label>
-        <mat-select [(ngModel)]="mdc_text_button_label_text_color">
-          <mat-option value="">Default</mat-option>
-          <mat-option value="red">red</mat-option>
-          <mat-option value="blue">blue</mat-option>
-          <mat-option value="green">green</mat-option>
-        </mat-select>
-      </mat-form-field>
+    <mat-divider />
 
-      <mat-form-field>
-        <mat-label>mat_text_button_horizontal_padding</mat-label>
-        <mat-select [(ngModel)]="mat_text_button_horizontal_padding">
-          <mat-option value="">Default</mat-option>
-          <mat-option value="0">0</mat-option>
-          <mat-option value="4px">4px</mat-option>
-          <mat-option value='12px'>12px</mat-option>
-        </mat-select>
-      </mat-form-field>
-    </section>
-  `,
-  styles: `
-    :host {
-      //--mat-text-button-horizontal-padding: '4px;',
-    }
+    <app-basic-controls />
   `,
   host: {
-      '[style.--mdc-text-button-label-text-color]': "mdc_text_button_label_text_color()",
-      '[style.--mat-text-button-touch-target-display]': "mat_text_button_touch_target_display()",
-      '[style.--mat-text-button-horizontal-padding]': "mat_text_button_horizontal_padding()",
       '[style.--mdc-outlined-button-label-text-color]': "mdc_outlined_button_label_text_color()",
       '[style.--mdc-outlined-button-label-text-size]': "mdc_outlined_button_label_text_size()",
       '[style.--mdc-filled-button-label-text-size]': "mdc_filled_button_label_text_size()",
@@ -81,11 +47,8 @@ import {MatOption, MatSelect} from "@angular/material/select";
 })
 export class ButtonComponent {
   // empty string makes the var the default from Material
+  basicControls = model<string[]>([])
 
-  // basic
-  mdc_text_button_label_text_color = signal('');
-  mat_text_button_touch_target_display = signal(''); // useless?
-  mat_text_button_horizontal_padding = signal('');
 
   // outlined
   mdc_outlined_button_label_text_color = signal('');
---
+++ src/app/button/controls/basic-controls.component.ts
@@ -0,0 +1,69 @@
+import {Component, signal} from '@angular/core';
+import {MatFormField} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {FormsModule} from "@angular/forms";
+import {MatAnchor, MatButton} from "@angular/material/button";
+
+
+@Component({
+  selector: 'app-basic-controls',
+  standalone: true,
+  imports: [
+    MatFormField,
+    MatSelectModule,
+    FormsModule,
+    MatAnchor,
+    MatButton
+  ],
+  template: `
+    <section id="Basic Button">
+      <h2>Basic Button Settings</h2>
+
+      <div>
+        <button mat-button>Basic</button>
+        <button mat-button disabled>Disabled</button>
+        <a mat-button href="https://www.google.com/" target="_blank">Link</a>
+      </div>
+
+      <mat-form-field>
+        <mat-label>mdc_text_button_label_text_color</mat-label>
+        <mat-select [(ngModel)]="$basicControls.mdc_text_button_label_text_color">
+          <mat-option value="">Default</mat-option>
+          <mat-option value="red">red</mat-option>
+          <mat-option value="blue">blue</mat-option>
+          <mat-option value="green">green</mat-option>
+        </mat-select>
+      </mat-form-field>
+
+      <mat-form-field>
+        <mat-label>mat_text_button_horizontal_padding</mat-label>
+        <mat-select [(ngModel)]="$basicControls.mat_text_button_horizontal_padding">
+          <mat-option value="">Default</mat-option>
+          <mat-option value="0">0</mat-option>
+          <mat-option value="4px">4px</mat-option>
+          <mat-option value='12px'>12px</mat-option>
+
+          @let randomPx = $random() + 'px' ;
+          <mat-option [value]='randomPx'>Random: {{ randomPx }}</mat-option>
+        </mat-select>
+      </mat-form-field>
+    </section>
+  `,
+  styles: `
+    :host {
+      //--mat-text-button-horizontal-padding: '4px;',
+    }
+  `,
+  host: {
+    '[style.--mdc-text-button-label-text-color]': "$basicControls.mdc_text_button_label_text_color()",
+    '[style.--mat-text-button-horizontal-padding]': "$basicControls.mat_text_button_horizontal_padding()",
+  }
+})
+export class BasicControlsComponent {
+  $random = signal(String(Math.floor(Math.random() * 100)))
+
+  $basicControls = {
+    mdc_text_button_label_text_color: signal(''),
+    mat_text_button_horizontal_padding: signal('')
+  }
+}
